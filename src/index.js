b742134a99cbbfb4a05792d690ed13e1d4c0a89a
Juan Carlos Hoyos Cabarique <juan.hoyos@integrait.co>
2023-11-26 18:29:01 -0500

    add responsive header

--- src/index.js
+++ src/index.js
@@ -12,26 +12,31 @@ const toggle = document.getElementById('toggle');
 
 const changeTheme = () => {
     const theme = localStorage.getItem('theme') ?? 'light';
+
     if (theme === 'dark') {
         document.documentElement.classList.remove('dark')
-        toggle.classList.add('translate-x-12');
+        toggle.classList.add('translate-x-0');
+        toggle.classList.remove('translate-x-12');
         localStorage.setItem('theme', 'light');
     } else {
         document.documentElement.classList.add('dark')
-        toggle.classList.remove('translate-x-12');
+        toggle.classList.add('translate-x-12');
+        toggle.classList.add('translate-x-0');
         localStorage.setItem('theme', 'dark');
     }
 }
 
 const main = () => {
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    const theme = localStorage.getItem('theme') ?? 'dark';
+    const theme = localStorage.getItem('theme');
     if (theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark')
-        toggle.classList.remove('translate-x-12');
+        toggle.classList.add('translate-x-12');
+        localStorage.setItem('theme', 'dark');
     } else {
         document.documentElement.classList.remove('dark')
-        toggle.classList.add('translate-x-12');
+        toggle.classList.remove('translate-x-12');
+        localStorage.setItem('theme', 'light');
     }
 }
 
